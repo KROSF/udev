e604e2c24b23b10718bde8a14257389c5850ff5b
KROSF <rodrigosanabria22@gmail.com>
2020-05-03 16:28:56 +0200

    fix: send 401 status code on unauthorized

--- app/Filters/JWTFilter.php
+++ app/Filters/JWTFilter.php
@@ -3,34 +3,48 @@
 namespace App\Filters;
 
 use App\Models\UserModel;
+use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\ResponseInterface;
-use CodeIgniter\Security\Exceptions\SecurityException;
 use Config\Auth;
+use Config\Services;
 use Firebase\JWT\JWT;
 
 class JWTFilter implements FilterInterface {
+  use ResponseTrait;
+
+  protected $response;
+
+  protected $request;
   /**
    *
    * @param IncomingRequest $request
    * @return mixed
    */
   public function before(RequestInterface $request) {
+    $this->request = $request;
+    $this->response = Services::response();
     if ($request->hasHeader('Authorization')) {
       /** @var Auth $authConfig */
       $authConfig = new Auth();
       $userModel = new UserModel();
       $authorization = $request->getHeader('Authorization');
-      $userID = JWT::decode($authorization->getValueLine(), $authConfig->jwtKey, [$authConfig->jwtAlgorithm]);
-      $user = $userModel->find($userID);
-      $request->user = $user;
 
-      return $request;
+      try {
+        $userID = JWT::decode($authorization->getValueLine(), $authConfig->jwtKey, [$authConfig->jwtAlgorithm]);
+        $user = $userModel->find($userID);
+        $request->user = $user;
+
+        return $request;
+      } catch (\Exception $th) {
+      }
     }
 
-    throw new SecurityException("Authorization header missing");
+    $this->failUnauthorized();
+    $this->response->send();
+    exit();
   }
 
   public function after(RequestInterface $request, ResponseInterface $response) {
