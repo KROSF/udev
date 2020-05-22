9b4d37cb4daa9dbff81c5eeed8244bcb27981fa3
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2020-05-22 21:43:51 +0200

    fix: add links to title

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,65 +1,65 @@
+/** @jsx jsx */
 import { Box, Flex, Image, Link } from '@chakra-ui/core'
 import React from 'react'
 import Gravatar from './Gravatar'
 import NextLink from 'next/link'
+import { Post } from '../services/api'
+import { css, jsx } from '@emotion/core'
 
 export interface CardProps {
-  image?: {
-    src: string
-    alt: string
-  }
-  title: string
-  author: {
-    email: string
-    name: string
-  }
-  tags: string[]
-  likes: number
-  comments: number
+  post: Post
+  first: boolean
 }
 
 const Card: React.FC<CardProps> = ({
-  image,
-  title,
-  author,
-  tags,
-  likes,
-  comments,
+  post: { url, title, tags, user, likes, comments, cover_url },
+  first,
 }) => {
   return (
     <Box borderWidth="1px" rounded="lg" width="100%" marginBottom={3}>
-      {image && <Image src={image.src} alt={image.alt} maxHeight="275px" />}
-      <Box p={6} borderTopWidth={image && '1px'}>
-        <Box
-          as="h3"
-          color="gray"
-          fontWeight="bold"
-          letterSpacing="normal"
-          fontSize="3xl"
+      {first && cover_url && <Image src={cover_url} alt="cover" />}
+      <Box p={6} borderTopWidth={first ? '1px' : undefined}>
+        <Link
+          href={url}
+          css={css`
+            &:hover {
+              text-decoration: none;
+            }
+          `}
         >
-          {title}
-        </Box>
+          <Box
+            as="h3"
+            color="gray"
+            fontWeight="bold"
+            letterSpacing="normal"
+            fontSize="3xl"
+          >
+            {title}
+          </Box>
+        </Link>
         <Flex flexDirection="row">
-          <Flex marginRight={1}>
-            <Gravatar email={author.email} />
-          </Flex>
+          <Link marginRight={1} href={`/users/${user.username}`}>
+            <Gravatar email={user.email} />
+          </Link>
           <Flex flexDirection="column">
             <Flex>
               {tags.map((tag) => (
-                <NextLink href={`/tags/${tag}`} key={tag}>
+                <NextLink href={`/tags/${tag.name}`} key={tag.id + tag.name}>
                   <Link as="a" marginRight={1}>
-                    #{tag}
+                    #{tag.name}
                   </Link>
                 </NextLink>
               ))}
             </Flex>
             <Flex>
-              <Link color="gray.500">{author.name}</Link>
+              <NextLink href={`/users/${user.username}`} passHref>
+                <Link color="gray.500">{user.name}</Link>
+              </NextLink>
             </Flex>
           </Flex>
         </Flex>
         <Flex marginTop={3}>
-          <Flex as={Link} marginRight={2}>
+          <Link display="flex" marginRight={2} href={url}>
             <Image
               src="/images/reactions-stack.png"
               alt="heart"
@@ -69,8 +69,8 @@ const Card: React.FC<CardProps> = ({
             <Box as="span" marginLeft={1} fontSize="base" color="gray.500">
               {likes}
             </Box>
-          </Flex>
-          <Flex as={Link}>
+          </Link>
+          <Link href={url} display="flex">
             <Image
               src="/images/comments-bubble.png"
               alt="chat"
@@ -80,7 +80,7 @@ const Card: React.FC<CardProps> = ({
             <Box as="span" marginLeft={1} fontSize="base" color="gray.500">
               {comments}
             </Box>
-          </Flex>
+          </Link>
         </Flex>
       </Box>
     </Box>
