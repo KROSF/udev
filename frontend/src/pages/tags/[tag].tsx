69f281cb8eeea4cb93e133904850e731c9a96997
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2020-05-22 21:34:07 +0200

    feat: show posts with a concrete tag

--- frontend/src/pages/tags/[tag].tsx
+++ frontend/src/pages/tags/[tag].tsx
@@ -1,27 +1,59 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { Flex, Button } from '@chakra-ui/core'
+import { RootPost } from '../../services/api'
+import { useApi } from '../../hooks'
+import Loading from '../../components/Loading'
+import Card from '../../components/Card'
 
 const Tag = () => {
   const router = useRouter()
   const { tag } = router.query
 
+  const { response, loading, error } = useApi<RootPost>({
+    url: `/tags/${tag}/posts`,
+    trigger: tag,
+    forceDispatchEffect: () => !!tag,
+  })
+
   return (
-    <Flex>
+    <Flex flexDirection="column" width="100%">
       <Flex
         borderWidth="2px"
         rounded="lg"
-        marginX="32px"
         paddingY="1.5rem"
         alignItems="center"
         justifyContent="center"
-        width="100%"
+        marginX="32px"
       >
         <Flex as="h1" letterSpacing="normal" fontSize="6xl" marginRight={4}>
           {tag}
         </Flex>
         <Button leftIcon="add">FOLLOW</Button>
       </Flex>
+      {loading && (
+        <Flex marginTop="1.5rem">
+          <Loading />
+        </Flex>
+      )}
+      {response !== null && (
+        <Flex marginTop="1.5rem">
+          <Flex flex={1} justifyContent="flex-end" paddingRight="1.5rem"></Flex>
+          <Flex flex={2} flexDirection="column">
+            {response.data.data.map((post, index) => {
+              return (
+                <Card
+                  first={index === 0}
+                  post={post}
+                  key={post.title + index}
+                />
+              )
+            })}
+          </Flex>
+          <Flex flex={1} paddingLeft="1.5rem"></Flex>
+        </Flex>
+      )}
+      {error && <Flex>Error...</Flex>}
     </Flex>
   )
 }
