4ae15defddfc96cd8552d235300a8463cea6700e
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2020-05-26 22:28:41 +0200

    feat: modified helper to get seo friendly urls

--- app/Helpers/frontend_helper.php
+++ app/Helpers/frontend_helper.php
@@ -14,6 +14,20 @@ if (!function_exists('frontEndBaseURL')) {
 
 if (!function_exists('postURL')) {
   function postURL(Post $post) {
-    return frontEndBaseURL([str_replace(' ', '-', strtolower($post->title)).'-'.$post->id]);
+    return frontEndBaseURL([cleanStr(strtolower($post->title).'-'.$post->id)]);
+  }
+}
+
+if (!function_exists('cleanStr')) {
+  function cleanStr($string){
+    // Replaces all spaces with hyphens.
+    $string = str_replace(' ', '-', $string);
+
+    // Removes special chars.
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+    // Replaces multiple hyphens with single one.
+    $string = preg_replace('/-+/', '-', $string);
+
+    return $string;
   }
 }
